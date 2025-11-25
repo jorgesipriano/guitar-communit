12c50fb399347f7912549dea0e06af59d589b8f6
jorge_sipriano <154374041+jorgesipriano@users.noreply.github.com>
2025-11-24 22:16:00 -0300

    Feat: Add Service Worker to enable PWA installation

--- script.js
+++ script.js
@@ -263,3 +263,15 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 });
+
+// Register Service Worker for PWA
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('./service-worker.js')
+            .then(registration => {
+                console.log('ServiceWorker registration successful');
+            }, err => {
+                console.log('ServiceWorker registration failed: ', err);
+            });
+    });
+}
